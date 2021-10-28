78032d8569447450242ae2e1e58a4b00e9bae3ae
santiagolenil@gmail.com <lenilson.santiago@sema.ma.gov.br>
2021-10-28 12:07:30 -0300

    Updates

--- core/Data.py
+++ core/Data.py
@@ -1,8 +1,8 @@
 services = [
 
-            {'title':'Dados Geográfico Completo', 'description':'Temos um banco de dados geográfico completo com todas as áreas especiais e hidrografia usadas para análise de sobreposição.', 'icon':'ic_data_base.png'},
-            {'title':'Diagnóstico de Sobreposição', 'description':'Através de nosso banco de dados geográfico verificamos com antecedência se seu imóvel está sobreposto a outras áreas e imóveis.', 'icon':'ic_sobre.png'},
-            {'title':'Análise da Cobertutra do Solo', 'description':'Imagens de satélites recentes e dados de classificação usadas para verificar a área consolidada e área remanescente de vegetação nativa.', 'icon':'ic_sobre_base.png'},
-            {'title':'Projeto de Proposta e Averbação de Reserva Legal', 'description':'Propomos um projecto de Reserva Legal (RL) usando todos nossos recursos e de acordo com a legislação Ambiental.', 'icon':'ic_rl.png'},
+            {'id':'data_geo', 'title':'Dados Geográfico Completo', 'description':'Temos um banco de dados geográfico completo com todas as áreas especiais e hidrografia usadas para análise de sobreposição.', 'icon':'ic_data_base.png', 'more':'Possuímos um banco de dados geográgico completo com todos os dados vetoriais usado para criar um diagnóstico completo de sobreposição. Verificamos atraves do nosso banco se seu imovél está sobreposto a áreas especiais. Bancos de Dados Geográficos: São banco de dados preparados para armazenar dados sobre informações espaciais.'},
+            {'id':'diagnostico_sobre','title':'Diagnóstico de Sobreposição', 'description':'Através de nosso banco de dados geográfico verificamos com antecedência se seu imóvel está sobreposto a outras áreas e imóveis.', 'icon':'ic_sobre.png', 'more':''},
+            {'id':'analise_soil','title':'Análise da Cobertutra do Solo', 'description':'Imagens de satélites recentes e dados de classificação usadas para verificar a área consolidada e área remanescente de vegetação nativa.', 'icon':'ic_sobre_base.png', 'more':''},
+            {'id':'propose_rl','title':'Projeto de Proposta e Averbação de Reserva Legal', 'description':'Propomos um projecto de Reserva Legal (RL) usando todos nossos recursos e de acordo com a legislação Ambiental.', 'icon':'ic_rl.png', 'more':''},
 
 ]
